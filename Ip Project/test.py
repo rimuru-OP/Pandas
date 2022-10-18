bc8df66246e705b3571e126a0fb1928f096f09b3
Annany Thakur <106261456+rimuru-OP@users.noreply.github.com>
2022-10-19 01:47:19 +0530

    Delete Ip Project directory

--- Ip Project/test.py
+++
@@ -1,8 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.animation as anim
-import math as mt
-import numpy as np
-import random as rd
-x = np.arange(0,10,0.01)
-plt.bar(x,np.e**x,color=rd.choices(["g","b","r","orange","yellow","black"],k=1001))
-plt.show()
